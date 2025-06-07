a6c16029e016f0c6919db934ae9e53b7270dddaf
ekp529 <ekpaupulaire@gmail.com>
2025-06-07 14:53:57 -0400

    completed final task

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+''' Executing this function initiates the application of emotion
+    detection to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,16 +10,24 @@ app = Flask("Emotion Detector")
 
 @app.route('/')
 def home():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template('index.html')
 
 @app.route('/emotionDetector')
 def use_emotion_detector():
+    ''' This code receives the text from the HTML interface and 
+        runs emotion detector over it using the emotion_detector()
+        function. The output returned shows several emotions, their confidence 
+        score, and the dominant emotion for the provided text.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     detection = emotion_detector(text_to_analyze)
 
     if detection['dominant_emotion'] is None:
         return 'Invalid text! Please try again.'
-        
+
     return f"For the given statement, the system response is \
     'anger': {detection['anger']}, \
     'disgust': {detection['disgust']}, \
